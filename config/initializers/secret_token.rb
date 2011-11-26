0db06609b430a90c53f98322d324cdfdecd47902
Neil Panjwani <paniwani@gmail.com>
2011-11-25 21:19:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prize::Application.config.secret_token = 'ae3ec1e5521947ceb55623598539e14a736306f520b335adb2a5c3c600f64dea443aea583f31d8d46b443bda8f7da7d4b1745f3fd493cbabea709347cac5c97d'
